7f586501105b277d833af78cfbc7e34ecf678c61
snoyer <noyer.stephane@gmail.com>
2026-02-22 11:30:33 +0400

    test splash screen

--- boards/shields/nice_view_sno1/src/testing_states.py
+++ boards/shields/nice_view_sno1/src/testing_states.py
@@ -6,6 +6,9 @@ from typing import Literal
 
 
 def demo_states(CONFIG_TRACKED_PROFILE_COUNT: int):
+    g = "splash"
+    yield state(g, "ble", "ble", 1, 1, {})
+
     g = "demo"
     yield state(g, "ble", "ble", 1, 1, {0: 1, 1: -1}, ("80+", "-1"), "NCs", "layer 1")
     yield state(
